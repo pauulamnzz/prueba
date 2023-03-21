4ae33fd8405945a6965c5f4130c8c34dddd9866b
Paula <pauula.mnzz@gmail.com>
2023-03-21 12:45:48 +0100

    He añadido un comentario al principio
    Signed-off-by: Paula <pauula.mnzz@gmail.com>

---
+++ tema3_ejercicio10/src/tema3_ejercicio10/Tema3_Ejercicio10.java
@@ -0,0 +1,31 @@
+/*
+ * Prueba GIT 
+ */
+package tema3_ejercicio10;
+
+import java.util.Scanner;
+
+public class Tema3_Ejercicio10 {
+
+	public static void main(String[] args) {
+		double ne1, ne2, ne3, nmedia;
+		Scanner m = new Scanner(System.in);
+		try {
+			System.out.print("Escriba su nota de la primera evaluación: ");
+			ne1 = m.nextDouble();
+			System.out.print("Escriba su nota de la segunda evaluación: ");
+			ne2 = m.nextDouble();
+			System.out.print("Escriba su nota de la tercera evaluación: ");
+			ne3 = m.nextDouble();
+
+			nmedia = (ne1 + ne2 + ne3) / 3;
+			System.out.println("Su nota media es: " + nmedia);
+		}catch(NumberFormatException e) {
+			System.out.println("ERROR: "+e.getMessage());
+		} 
+		
+		
+		
+	}
+
+}
